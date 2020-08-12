54cd0848082679a7b093381c4841f278a0583e02
sunshineplan <sunshineplan@users.noreply.github.com>
2020-08-12 14:10:24 +0800

    Add export CSV with UTF8-BOM function

--- export/csv.go
+++ export/csv.go
@@ -6,13 +6,14 @@ import (
 	"io"
 )
 
-// CSV export csv content to writer
-func CSV(fieldnames []string, rows []interface{}, writer io.Writer) error {
-	w := csv.NewWriter(writer)
-	if err := w.Write(fieldnames); err != nil {
+func exportCSV(fieldnames []string, rows []interface{}, w io.Writer, utf8bom bool) error {
+	if utf8bom {
+		w.Write([]byte{0xEF, 0xBB, 0xBF})
+	}
+	writer := csv.NewWriter(w)
+	if err := writer.Write(fieldnames); err != nil {
 		return err
 	}
-
 	for _, row := range rows {
 		m, ok := row.(map[string]interface{})
 		if !ok {
@@ -24,15 +25,23 @@ func CSV(fieldnames []string, rows []interface{}, writer io.Writer) error {
 				r[index] = fmt.Sprintf("%v", m[fieldname])
 			}
 		}
-		if err := w.Write(r); err != nil {
+		if err := writer.Write(r); err != nil {
 			return err
 		}
 	}
-
-	w.Flush()
-	if err := w.Error(); err != nil {
+	writer.Flush()
+	if err := writer.Error(); err != nil {
 		return err
 	}
-
 	return nil
+}
+
+// CSV export csv content to writer
+func CSV(fieldnames []string, rows []interface{}, w io.Writer) error {
+	return exportCSV(fieldnames, rows, w, false)
+}
+
+// CSVWithUTF8BOM export csv content to writer
+func CSVWithUTF8BOM(fieldnames []string, rows []interface{}, w io.Writer) error {
+	return exportCSV(fieldnames, rows, w, true)
 }
