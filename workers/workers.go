379b7918deccf49ad521217ee12e5f1fea46947c
sunshineplan <sunshineplan@users.noreply.github.com>
2020-08-15 10:14:58 +0800

    Workers add RunRange function

--- workers/workers.go
+++ workers/workers.go
@@ -21,3 +21,15 @@ func (w *Workers) Run(items []interface{}, runner func(chan bool, int, interface
 		c <- true
 	}
 }
+
+// RunRange run workers according range
+func (w *Workers) RunRange(start, end int, runner func(chan bool, int)) {
+	c := make(chan bool, w.Max)
+	for i := start; i <= end; i++ {
+		c <- true
+		go runner(c, i)
+	}
+	for i := 0; i < w.Max; i++ {
+		c <- true
+	}
+}
