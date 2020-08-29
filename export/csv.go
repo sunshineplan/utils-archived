c8405fceac64c6f10794a60179be547dd1b4227f
sunshineplan <sunshineplan@users.noreply.github.com>
2020-08-29 10:17:08 +0800

    Export csv support struct slice

--- export/csv.go
+++ export/csv.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"io"
+	"reflect"
 )
 
 func exportCSV(fieldnames []string, rows []interface{}, w io.Writer, utf8bom bool) error {
@@ -15,15 +16,26 @@ func exportCSV(fieldnames []string, rows []interface{}, w io.Writer, utf8bom boo
 		return err
 	}
 	for _, row := range rows {
-		m, ok := row.(map[string]interface{})
-		if !ok {
-			return fmt.Errorf("can not convert row to map[string]interface{}")
-		}
 		r := make([]string, len(fieldnames))
-		for index, fieldname := range fieldnames {
-			if m[fieldname] != nil {
-				r[index] = fmt.Sprintf("%v", m[fieldname])
+		switch obj := reflect.ValueOf(row); obj.Kind() {
+		case reflect.Map:
+			if reflect.TypeOf(row).Key().Name() == "string" {
+				for index, fieldname := range fieldnames {
+					if v := obj.MapIndex(reflect.ValueOf(fieldname)); v.IsValid() {
+						r[index] = fmt.Sprintf("%v", v)
+					}
+				}
+			} else {
+				return fmt.Errorf("can not export rows which map is not string")
 			}
+		case reflect.Struct:
+			for index, fieldname := range fieldnames {
+				if v := obj.FieldByName(fieldname); v.IsValid() {
+					r[index] = fmt.Sprintf("%v", v)
+				}
+			}
+		default:
+			return fmt.Errorf("not support rows format: %s", obj.Kind())
 		}
 		if err := writer.Write(r); err != nil {
 			return err
---
+++ export/csv_test.go
@@ -0,0 +1,52 @@
+package export
+
+import (
+	"bytes"
+	"io/ioutil"
+	"reflect"
+	"testing"
+)
+
+func TestCSV(t *testing.T) {
+	type test struct{ A, B string }
+	mapString := []interface{}{map[string]interface{}{"A": "a", "B": "b"}, map[string]interface{}{"A": "aa", "B": "bb"}}
+	Struct := []interface{}{test{A: "a", B: "b"}, test{A: "aa", B: "bb"}}
+	result := `A,B
+a,b
+aa,bb
+`
+
+	var b1, b2 bytes.Buffer
+	if CSV([]string{"A", "B"}, mapString, &b1) != nil {
+		t.Error("Export map source csv failed")
+	}
+	if CSV([]string{"A", "B"}, Struct, &b2) != nil {
+		t.Error("Export struct source csv failed")
+	}
+	c1, _ := ioutil.ReadAll(&b1)
+	c2, _ := ioutil.ReadAll(&b2)
+	if string(c1) != result {
+		t.Error("Export map source csv result is not except one")
+	}
+	if string(c2) != result {
+		t.Error("Export struct source csv result is not except one")
+	}
+}
+
+func TestCSVWithUTF8BOM(t *testing.T) {
+	bom := []byte{0xEF, 0xBB, 0xBF}
+	result := `A,B
+a,b
+`
+	var b bytes.Buffer
+	if CSVWithUTF8BOM([]string{"A", "B"}, []interface{}{map[string]interface{}{"A": "a", "B": "b"}}, &b) != nil {
+		t.Error("Export csv with utf8bom failed")
+	}
+	c, _ := ioutil.ReadAll(&b)
+	if !reflect.DeepEqual(bom, c[:3]) {
+		t.Error("Export csv with utf8bom result not contain bom header")
+	}
+	if string(c[3:]) != result {
+		t.Error("Export csv with utf8bom result is not except one")
+	}
+}
