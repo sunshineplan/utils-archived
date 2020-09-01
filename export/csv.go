d78ec8480ccedd1b18914f51ccb66c42d98012d3
sunshineplan <sunshineplan@users.noreply.github.com>
2020-09-01 13:17:33 +0800

    Export csv use interface{} as argument directly

--- export/csv.go
+++ export/csv.go
@@ -7,7 +7,7 @@ import (
 	"reflect"
 )
 
-func exportCSV(fieldnames []string, rows []interface{}, w io.Writer, utf8bom bool) error {
+func exportCSV(fieldnames []string, slice interface{}, w io.Writer, utf8bom bool) error {
 	if utf8bom {
 		w.Write([]byte{0xEF, 0xBB, 0xBF})
 	}
@@ -15,13 +15,21 @@ func exportCSV(fieldnames []string, rows []interface{}, w io.Writer, utf8bom boo
 	if err := writer.Write(fieldnames); err != nil {
 		return err
 	}
-	for _, row := range rows {
+	if reflect.TypeOf(slice).Kind() != reflect.Slice {
+		return fmt.Errorf("rows is not slice")
+	}
+	rows := reflect.ValueOf(slice)
+	for i := 0; i < rows.Len(); i++ {
+		row := rows.Index(i)
+		if row.Kind() == reflect.Interface {
+			row = row.Elem()
+		}
 		r := make([]string, len(fieldnames))
-		switch obj := reflect.ValueOf(row); obj.Kind() {
+		switch row.Kind() {
 		case reflect.Map:
-			if reflect.TypeOf(row).Key().Name() == "string" {
+			if reflect.TypeOf(row.Interface()).Key().Name() == "string" {
 				for index, fieldname := range fieldnames {
-					if v := obj.MapIndex(reflect.ValueOf(fieldname)); v.IsValid() && v.Interface() != nil {
+					if v := row.MapIndex(reflect.ValueOf(fieldname)); v.IsValid() && v.Interface() != nil {
 						r[index] = fmt.Sprintf("%v", v)
 					}
 				}
@@ -30,12 +38,12 @@ func exportCSV(fieldnames []string, rows []interface{}, w io.Writer, utf8bom boo
 			}
 		case reflect.Struct:
 			for index, fieldname := range fieldnames {
-				if v := obj.FieldByName(fieldname); v.IsValid() && v.Interface() != nil {
+				if v := row.FieldByName(fieldname); v.IsValid() && v.Interface() != nil {
 					r[index] = fmt.Sprintf("%v", v)
 				}
 			}
 		default:
-			return fmt.Errorf("not support rows format: %s", obj.Kind())
+			return fmt.Errorf("not support rows format: %s", row.Kind())
 		}
 		if err := writer.Write(r); err != nil {
 			return err
@@ -49,11 +57,11 @@ func exportCSV(fieldnames []string, rows []interface{}, w io.Writer, utf8bom boo
 }
 
 // CSV export csv content to writer
-func CSV(fieldnames []string, rows []interface{}, w io.Writer) error {
-	return exportCSV(fieldnames, rows, w, false)
+func CSV(fieldnames []string, slice interface{}, w io.Writer) error {
+	return exportCSV(fieldnames, slice, w, false)
 }
 
 // CSVWithUTF8BOM export csv content to writer
-func CSVWithUTF8BOM(fieldnames []string, rows []interface{}, w io.Writer) error {
-	return exportCSV(fieldnames, rows, w, true)
+func CSVWithUTF8BOM(fieldnames []string, slice interface{}, w io.Writer) error {
+	return exportCSV(fieldnames, slice, w, true)
 }
--- export/csv_test.go
+++ export/csv_test.go
@@ -2,6 +2,7 @@ package export
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"reflect"
 	"testing"
@@ -9,27 +10,38 @@ import (
 
 func TestCSV(t *testing.T) {
 	type test struct{ A, B interface{} }
-	mapString := []interface{}{map[string]interface{}{"A": "a", "B": "b"}, map[string]interface{}{"A": "aa", "B": nil}}
-	Struct := []interface{}{test{A: "a", B: "b"}, test{A: "aa", B: nil}}
+	mapSlice := []map[string]interface{}{map[string]interface{}{"A": "a", "B": "b"}, map[string]interface{}{"A": "aa", "B": nil}}
+	structSlice := []test{test{A: "a", B: "b"}, test{A: "aa", B: nil}}
+	interfaceSlice := []interface{}{test{A: "a", B: "b"}, map[string]interface{}{"A": "aa", "B": nil}}
 	result := `A,B
 a,b
 aa,
 `
 
-	var b1, b2 bytes.Buffer
-	if CSV([]string{"A", "B"}, mapString, &b1) != nil {
-		t.Error("Export map source csv failed")
+	var b1, b2, b3 bytes.Buffer
+	if err := CSV([]string{"A", "B"}, mapSlice, &b1); err != nil {
+		fmt.Println(err)
+		t.Error("Export map slice source csv failed")
 	}
-	if CSV([]string{"A", "B"}, Struct, &b2) != nil {
-		t.Error("Export struct source csv failed")
+	if err := CSV([]string{"A", "B"}, structSlice, &b2); err != nil {
+		fmt.Println(err)
+		t.Error("Export struct slice source csv failed")
+	}
+	if err := CSV([]string{"A", "B"}, interfaceSlice, &b3); err != nil {
+		fmt.Println(err)
+		t.Error("Export interface slice source csv failed")
 	}
 	c1, _ := ioutil.ReadAll(&b1)
 	c2, _ := ioutil.ReadAll(&b2)
+	c3, _ := ioutil.ReadAll(&b3)
 	if string(c1) != result {
-		t.Error("Export map source csv result is not except one")
+		t.Error("Export map slice source csv result is not except one")
 	}
 	if string(c2) != result {
-		t.Error("Export struct source csv result is not except one")
+		t.Error("Export struct slice source csv result is not except one")
+	}
+	if string(c3) != result {
+		t.Error("Export interface slice source csv result is not except one")
 	}
 }
 
@@ -39,7 +51,8 @@ func TestCSVWithUTF8BOM(t *testing.T) {
 a,b
 `
 	var b bytes.Buffer
-	if CSVWithUTF8BOM([]string{"A", "B"}, []interface{}{map[string]interface{}{"A": "a", "B": "b"}}, &b) != nil {
+	if err := CSVWithUTF8BOM([]string{"A", "B"}, []interface{}{map[string]string{"A": "a", "B": "b"}}, &b); err != nil {
+		fmt.Println(err)
 		t.Error("Export csv with utf8bom failed")
 	}
 	c, _ := ioutil.ReadAll(&b)
