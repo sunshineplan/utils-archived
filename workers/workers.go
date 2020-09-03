b666925152806d7ed9106e0671b5b2ac8dd82351
sunshineplan <sunshineplan@users.noreply.github.com>
2020-09-03 21:54:18 +0800

    Workers add run on map

--- workers/workers.go
+++ workers/workers.go
@@ -23,28 +23,33 @@ func SetMax(max int) {
 	defaultWorkers.Max = max
 }
 
-// Run default workers on slice
-func Run(slice interface{}, runner func(int, interface{})) error {
-	return defaultWorkers.Run(slice, runner)
+// RunOnSlice default workers on slice
+func RunOnSlice(Slice interface{}, Runner func(int, interface{})) error {
+	return defaultWorkers.RunOnSlice(Slice, Runner)
 }
 
-// RunRange run default workers on range
-func RunRange(start, end int, runner func(int)) error {
-	return defaultWorkers.RunRange(start, end, runner)
+// RunOnMap default workers on slice
+func RunOnMap(Map interface{}, Runner func(interface{}, interface{})) error {
+	return defaultWorkers.RunOnMap(Map, Runner)
 }
 
-// Run workers on slice
-func (w Workers) Run(slice interface{}, runner func(int, interface{})) error {
-	if reflect.TypeOf(slice).Kind() != reflect.Slice {
+// RunOnRange run default workers on range
+func RunOnRange(Start, End int, Runner func(int)) error {
+	return defaultWorkers.RunOnRange(Start, End, Runner)
+}
+
+// RunOnSlice workers on slice
+func (w Workers) RunOnSlice(Slice interface{}, Runner func(int, interface{})) error {
+	if reflect.TypeOf(Slice).Kind() != reflect.Slice {
 		return fmt.Errorf("First argument must be a slice")
 	}
-	values := reflect.ValueOf(slice)
+	values := reflect.ValueOf(Slice)
 	c := make(chan bool, w.Max)
 	for i := 0; i < values.Len(); i++ {
 		c <- true
 		go func(index int, value interface{}) {
 			defer func() { <-c }()
-			runner(index, value)
+			Runner(index, value)
 		}(i, values.Index(i).Interface())
 	}
 	for i := 0; i < w.Max; i++ {
@@ -53,14 +58,36 @@ func (w Workers) Run(slice interface{}, runner func(int, interface{})) error {
 	return nil
 }
 
-// RunRange run workers on range
-func (w Workers) RunRange(start, end int, runner func(int)) error {
+// RunOnMap workers on slice
+func (w Workers) RunOnMap(Map interface{}, Runner func(interface{}, interface{})) error {
+	if reflect.TypeOf(Map).Kind() != reflect.Map {
+		return fmt.Errorf("First argument must be a map")
+	}
+	iter := reflect.ValueOf(Map).MapRange()
 	c := make(chan bool, w.Max)
-	for i := start; i <= end; i++ {
+	for iter.Next() {
+		k := iter.Key()
+		v := iter.Value()
+		c <- true
+		go func(k, v interface{}) {
+			defer func() { <-c }()
+			Runner(k, v)
+		}(k.Interface(), v.Interface())
+	}
+	for i := 0; i < w.Max; i++ {
+		c <- true
+	}
+	return nil
+}
+
+// RunOnRange run workers on range
+func (w Workers) RunOnRange(Start, End int, Runner func(int)) error {
+	c := make(chan bool, w.Max)
+	for i := Start; i <= End; i++ {
 		c <- true
 		go func(num int) {
 			defer func() { <-c }()
-			runner(num)
+			Runner(num)
 		}(i)
 	}
 	for i := 0; i < w.Max; i++ {
--- workers/workers_test.go
+++ workers/workers_test.go
@@ -3,7 +3,9 @@ package workers
 import (
 	"fmt"
 	"reflect"
+	"sort"
 	"strings"
+	"sync"
 	"testing"
 )
 
@@ -12,37 +14,60 @@ func TestSetMax(t *testing.T) {
 	if defaultWorkers.Max != 5 {
 		t.Error("SetMax result is not except one")
 	}
+	SetMax(100)
+	if defaultWorkers.Max != 100 {
+		t.Error("SetMax result is not except one")
+	}
 }
 
-func TestRun(t *testing.T) {
+func TestRunOnSlice(t *testing.T) {
 	type test struct {
 		char  string
 		times int
 	}
-	items := []test{test{"a", 1}, test{"b", 2}, test{"c", 3}}
-	result := make([]string, len(items))
-	if err := Run(items, func(i int, item interface{}) {
+	Slice := []test{test{"a", 1}, test{"b", 2}, test{"c", 3}}
+	result := make([]string, len(Slice))
+	if err := RunOnSlice(Slice, func(i int, item interface{}) {
 		result[i] = strings.Repeat(item.(test).char, item.(test).times)
 	}); err != nil {
 		fmt.Println(err)
-		t.Error("Run workers failed")
+		t.Error("RunOnSlice workers failed")
 	}
 	if !reflect.DeepEqual(result, []string{"a", "bb", "ccc"}) {
-		t.Error("Run workers result is not except one")
+		t.Error("RunOnSlice workers result is not except one")
 	}
 }
 
-func TestRunRange(t *testing.T) {
+func TestRunOnMap(t *testing.T) {
+	Map := map[string]int{"a": 1, "b": 2, "c": 3}
+	m := &sync.Mutex{}
+	var result []string
+	if err := RunOnMap(Map, func(k, v interface{}) {
+		m.Lock()
+		result = append(result, strings.Repeat(k.(string), v.(int)))
+		m.Unlock()
+	}); err != nil {
+		fmt.Println(err)
+		t.Error("RunOnMap workers failed")
+	}
+	sort.Strings(result)
+	if !reflect.DeepEqual(result, []string{"a", "bb", "ccc"}) {
+		fmt.Println(result)
+		t.Error("RunOnMap workers result is not except one")
+	}
+}
+
+func TestRunOnRange(t *testing.T) {
 	end := 3
 	items := []string{"a", "b", "c"}
 	result := make([]string, end)
-	if err := RunRange(1, end, func(num int) {
+	if err := RunOnRange(1, end, func(num int) {
 		result[num-1] = strings.Repeat(items[num-1], num)
 	}); err != nil {
 		fmt.Println(err)
-		t.Error("RunRange workers failed")
+		t.Error("RunOnRange workers failed")
 	}
 	if !reflect.DeepEqual(result, []string{"a", "bb", "ccc"}) {
-		t.Error("RunRange workers result is not except one")
+		t.Error("RunOnRange workers result is not except one")
 	}
 }
