4c4886ed6f183cd24805acebe391d52411d3990c
sunshineplan <sunshineplan@users.noreply.github.com>
2020-10-10 10:53:49 +0800

    Update progressbar

--- progressbar/progressbar.go
+++ progressbar/progressbar.go
@@ -4,9 +4,10 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"log"
+	"io/ioutil"
 	"os"
 	"strings"
+	"sync"
 	"text/template"
 	"time"
 )
@@ -16,11 +17,14 @@ const defaultTemplate = `[{{.Done}}{{.Undone}}]   {{printf "%.2f/s" .Speed}} - {
 
 // ProgressBar is a simple progress bar
 type ProgressBar struct {
-	width    int
-	refresh  time.Duration
-	template *template.Template
-	done     chan bool
-	last     int
+	sync.Mutex
+	blockWidth int
+	refresh    time.Duration
+	template   *template.Template
+	current    int
+	total      int
+	done       chan bool
+	lastWidth  int
 }
 
 type format struct {
@@ -33,90 +37,108 @@ type format struct {
 func (f *format) execute(pb *ProgressBar) {
 	var buf bytes.Buffer
 	pb.template.Execute(&buf, f)
-	last := buf.Len()
-	if last < pb.last {
+	width := buf.Len()
+	if width < pb.lastWidth {
 		io.WriteString(os.Stderr,
-			fmt.Sprintf("\r%s\r%s", strings.Repeat(" ", pb.last), buf.Bytes()))
+			fmt.Sprintf("\r%s\r%s", strings.Repeat(" ", pb.lastWidth), buf.Bytes()))
 	} else {
 		io.WriteString(os.Stderr, "\r\r"+string(buf.Bytes()))
 	}
 }
 
 // New returns a new ProgressBar with default options
-func New(c chan bool) *ProgressBar {
+func New(total int, done chan bool) *ProgressBar {
 	return &ProgressBar{
-		width:    50,
-		refresh:  5 * time.Second,
-		template: template.Must(template.New("ProgressBar").Parse(defaultTemplate)),
-		done:     c,
+		blockWidth: 50,
+		refresh:    5 * time.Second,
+		template:   template.Must(template.New("ProgressBar").Parse(defaultTemplate)),
+		total:      total,
+		done:       done,
 	}
 }
 
-// SetWidth sets progress bar width
-func (pb *ProgressBar) SetWidth(width int) *ProgressBar {
-	pb.width = width
+// SetWidth sets progress bar block width
+func (pb *ProgressBar) SetWidth(blockWidth int) *ProgressBar {
+	pb.blockWidth = blockWidth
 	return pb
 }
 
-// SetRefresh sets progress bar refresh
+// SetRefresh sets progress bar refresh time for check speed
 func (pb *ProgressBar) SetRefresh(refresh time.Duration) *ProgressBar {
 	pb.refresh = refresh
 	return pb
 }
 
 // SetTemplate sets progress bar template
-func (pb *ProgressBar) SetTemplate(format string) *ProgressBar {
+func (pb *ProgressBar) SetTemplate(tmplt string) error {
 	t := template.New("ProgressBar")
-	if _, err := t.Parse(format); err != nil {
-		log.Print("Invalid template.")
-		return pb
+	if _, err := t.Parse(tmplt); err != nil {
+		return err
+	}
+	if err := t.Execute(ioutil.Discard, format{}); err != nil {
+		return err
 	}
 	pb.template = t
-	return pb
+	return nil
+}
+
+// Add adds the specified amount to the progress bar
+func (pb *ProgressBar) Add(num int) {
+	pb.Lock()
+	defer pb.Unlock()
+	pb.current += num
 }
 
 // Start ProgressBar
-func (pb *ProgressBar) Start(total int, current *int) {
-	var speed float64
+func (pb *ProgressBar) Start() {
 	go func() {
-		for {
-			now := *current
-			if now >= total {
-				return
+		var speed float64
+		go func() {
+			for {
+				now := pb.current
+				if now >= pb.total {
+					return
+				}
+				time.Sleep(pb.refresh)
+				speed = float64(pb.current-now) / float64(pb.refresh/time.Second)
 			}
-			time.Sleep(pb.refresh)
-			speed = float64(*current-now) / float64(pb.refresh/time.Second)
-		}
-	}()
-	go func() {
-		for {
-			start := time.Now()
-			now := *current
-			done := pb.width * now / total
-			percent := float64(now) * 100 / float64(total)
-			left := time.Duration(float64(total-now)/speed) * time.Second
-			var progressed string
-			if now < total && done != 0 {
-				progressed = strings.Repeat("=", done-1) + ">"
-			} else {
-				progressed = strings.Repeat("=", done)
+		}()
+		go func() {
+			for {
+				start := time.Now()
+				now := pb.current
+				if now > pb.total {
+					now = pb.total
+				}
+				done := pb.blockWidth * now / pb.total
+				percent := float64(now) * 100 / float64(pb.total)
+				left := time.Duration(float64(pb.total-now)/speed) * time.Second
+				if left < 0 {
+					left = 0
+				}
+				var progressed string
+				if now < pb.total && done != 0 {
+					progressed = strings.Repeat("=", done-1) + ">"
+				} else {
+					progressed = strings.Repeat("=", done)
+				}
+				f := format{
+					Done:    progressed,
+					Undone:  strings.Repeat(" ", pb.blockWidth-done),
+					Speed:   speed,
+					Current: now,
+					Percent: percent,
+					Total:   pb.total,
+					Left:    left,
+				}
+				f.execute(pb)
+				if now == pb.total {
+					io.WriteString(os.Stderr, "\n")
+					pb.done <- true
+					return
+				}
+				time.Sleep(time.Second - time.Since(start))
 			}
-			f := format{
-				Done:    progressed,
-				Undone:  strings.Repeat(" ", pb.width-done),
-				Speed:   speed,
-				Current: now,
-				Percent: percent,
-				Total:   total,
-				Left:    left,
-			}
-			f.execute(pb)
-			if now >= total {
-				io.WriteString(os.Stderr, "\n")
-				pb.done <- true
-				return
-			}
-			time.Sleep(time.Second - time.Since(start))
-		}
+		}()
 	}()
 }
--- progressbar/progressbar_test.go
+++ progressbar/progressbar_test.go
@@ -7,12 +7,22 @@ import (
 
 func TestProgessBar(t *testing.T) {
 	done := make(chan bool, 1)
-	var i int
-	pb := New(done)
-	go pb.Start(10, &i)
-	for ; i <= 10; i++ {
+	pb := New(15, done).SetRefresh(4 * time.Second)
+	pb.Start()
+	for i := 0; i < pb.total; i++ {
 		//log.Print(i)
+		pb.Add(1)
 		time.Sleep(time.Second)
 	}
 	<-done
+}
+
+func TestSetTemplate(t *testing.T) {
+	pb := ProgressBar{}
+	if err := pb.SetTemplate(`{{.Done}}`); err != nil {
+		t.Error("test except non error")
+	}
+	if err := pb.SetTemplate(`{{.Test}}`); err == nil {
+		t.Error("test except error")
+	}
 }
