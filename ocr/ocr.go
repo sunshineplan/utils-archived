96f0287944be450917fcb984d71e66988378a56c
sunshineplan <sunshineplan@users.noreply.github.com>
2020-10-28 21:08:43 +0800

    Remove requests package and add ocr package

---
+++ ocr/ocr.go
@@ -0,0 +1,64 @@
+package utils
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+)
+
+const (
+	api    = "https://api.ocr.space/parse/image"
+	apikey = "5a64d478-9c89-43d8-88e3-c65de9999580"
+)
+
+type ocrText struct {
+	ParsedResults []struct {
+		ParsedText string
+	}
+}
+
+// OCRFromReader convert image to text
+func OCRFromReader(data io.Reader) (string, error) {
+	params := map[string]string{
+		"isOverlayRequired":            "true",
+		"isSearchablePdfHideTextLayer": "true",
+		"scale":                        "true",
+	}
+
+	var body bytes.Buffer
+	w := multipart.NewWriter(&body)
+	defer w.Close()
+
+	part, _ := w.CreateFormFile("file", "pic.jpg")
+	if _, err := io.Copy(part, data); err != nil {
+		return "", err
+	}
+
+	for k, v := range params {
+		w.WriteField(k, v)
+	}
+
+	req, _ := http.NewRequest("POST", api, &body)
+	req.Header.Set("Content-Type", w.FormDataContentType())
+	req.Header.Set("apikey", apikey)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	b, _ := ioutil.ReadAll(resp.Body)
+	var results ocrText
+	if err := json.Unmarshal(b, &results); err != nil {
+		return "", err
+	}
+	if len(results.ParsedResults) == 0 {
+		return "", errors.New("no ocr result")
+	}
+	return results.ParsedResults[0].ParsedText, nil
+}
