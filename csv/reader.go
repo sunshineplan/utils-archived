48c35ae9a8f976e5ee80cca9291f416b066b9449
sunshineplan <sunshineplan@users.noreply.github.com>
2021-01-25 16:46:46 +0800

    CSV reader store content in channel

--- csv/reader.go
+++ csv/reader.go
@@ -1,6 +1,7 @@
 package csv
 
 import (
+	"bytes"
 	"encoding/csv"
 	"fmt"
 	"io"
@@ -10,30 +11,58 @@ import (
 // the first row of the result set. Use Next to advance from row to row.
 type Rows struct {
 	fields   []string
-	records  [][]string
-	length   int
-	position int
+	records  chan []string
+	lastcols []string
 	closed   bool
+}
+
+func lineCounter(r io.Reader) (count int, err error) {
+	buf := make([]byte, 32*1024)
+
+	for {
+		var c int
+		c, err = r.Read(buf)
+		if err == io.EOF {
+			err = nil
+			return
+		}
+		if err != nil {
+			return
+		}
+		count += bytes.Count(buf[:c], []byte{'\n'})
+	}
 }
 
 // ReadAll reads all the remaining records from r.
 func ReadAll(r io.Reader) (*Rows, error) {
-	records, err := csv.NewReader(r).ReadAll()
+	var buf bytes.Buffer
+	count, err := lineCounter(io.TeeReader(r, &buf))
 	if err != nil {
 		return nil, err
 	}
 
-	rs := &Rows{}
-	switch len(records) {
+	reader := csv.NewReader(&buf)
+	rs := &Rows{records: make(chan []string, count+1)}
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		rs.records <- record
+	}
+
+	switch len(rs.records) {
 	case 0:
 		return nil, fmt.Errorf("Empty csv file")
 	case 1:
-		rs.fields = records[0]
-		return rs, nil
+		rs.fields = <-rs.records
+		close(rs.records)
+		rs.closed = true
 	default:
-		rs.fields = records[0]
-		rs.records = records[1:]
-		rs.length = len(rs.records)
+		rs.fields = <-rs.records
 	}
 
 	return rs, nil
@@ -46,11 +75,18 @@ func (rs *Rows) Fields() []string {
 
 // Next prepares the next result row for reading with the Scan method.
 func (rs *Rows) Next() bool {
-	if rs.position < rs.length {
-		rs.position++
+	if rs.closed {
+		return false
+	}
+
+	if len(rs.records) > 0 {
+		rs.lastcols = <-rs.records
 		return true
 	}
+
+	close(rs.records)
 	rs.closed = true
+
 	return false
 }
 
@@ -60,13 +96,18 @@ func (rs *Rows) Scan(dest ...interface{}) error {
 	if rs.closed {
 		return fmt.Errorf("Rows are closed")
 	}
+
 	if len(dest) != len(rs.fields) {
 		return fmt.Errorf("expected %d destination arguments in Scan, not %d", len(rs.fields), len(dest))
 	}
 
-	for i, v := range rs.records[rs.position-1] {
+	if rs.lastcols == nil {
+		return fmt.Errorf("Scan called without calling Next")
+	}
+
+	for i, v := range rs.lastcols {
 		if err := convertAssign(dest[i], v); err != nil {
-			return fmt.Errorf(`Scan error on field index %d, name %q: %v`, i, rs.Fields()[i], err)
+			return fmt.Errorf("Scan error on field index %d, name %q: %v", i, rs.Fields()[i], err)
 		}
 	}
 
