e5590174da7da5ca24dfbb5156138a6e50365d04
sunshineplan <sunshineplan@users.noreply.github.com>
2021-01-29 21:32:14 +0800

    Mongodb add srv prefix

--- database/mongodb/mongodb.go
+++ database/mongodb/mongodb.go
@@ -20,36 +20,52 @@ type Config struct {
 	Collection string
 	Username   string
 	Password   string
+	SRV        bool
+}
+
+// URI returns mongodb uri connection string
+func (c *Config) URI() string {
+	var prefix string
+	if c.SRV {
+		prefix = "mongodb+srv"
+	} else {
+		prefix = "mongodb"
+	}
+
+	if c.Username == "" {
+		return fmt.Sprintf("%s://%s:%d/%s", prefix, c.Server, c.Port, c.Database)
+	}
+	return fmt.Sprintf("%s://%s:%s@%s:%d/%s", prefix, c.Username, c.Password, c.Server, c.Port, c.Database)
 }
 
 // Open opens a mongodb database.
 func (c *Config) Open() (*mongo.Client, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
-		fmt.Sprintf("mongodb://%s:%s@%s:%d/%s", c.Username, c.Password, c.Server, c.Port, c.Database)))
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(c.URI()))
 	if err != nil {
 		return nil, err
 	}
+
 	ctx, cancelPing := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancelPing()
+
 	err = client.Ping(ctx, readpref.Primary())
 	if err != nil {
 		return nil, err
 	}
+
 	return client, nil
 }
 
 // Backup backups mongodb database to file.
 func (c *Config) Backup(file string) error {
 	args := []string{}
-	args = append(args, fmt.Sprintf("-h%s:%d", c.Server, c.Port))
-	args = append(args, fmt.Sprintf("-d%s", c.Database))
-	args = append(args, fmt.Sprintf("-c%s", c.Collection))
-	args = append(args, fmt.Sprintf("-u%s", c.Username))
-	args = append(args, fmt.Sprintf("-p%s", c.Password))
+	args = append(args, fmt.Sprintf("--uri=%q", c.URI()))
+	args = append(args, "-c"+c.Collection)
 	args = append(args, "--gzip")
-	args = append(args, fmt.Sprintf("--archive=%s", file))
+	args = append(args, fmt.Sprintf("--archive=%q", file))
 
 	command := exec.Command("mongodump", args...)
 	var stderr bytes.Buffer
@@ -60,23 +76,20 @@ func (c *Config) Backup(file string) error {
 	return nil
 }
 
-// Restore restores mongodb database from file.
+// Restore restores mongodb database collection from file.
 func (c *Config) Restore(file string) error {
 	args := []string{}
-	args = append(args, fmt.Sprintf("-h%s:%d", c.Server, c.Port))
-	args = append(args, fmt.Sprintf("-d%s", c.Database))
-	args = append(args, fmt.Sprintf("-c%s", c.Collection))
-	args = append(args, fmt.Sprintf("-u%s", c.Username))
-	args = append(args, fmt.Sprintf("-p%s", c.Password))
+	args = append(args, fmt.Sprintf("--uri=%q", c.URI()))
+	args = append(args, "-c"+c.Collection)
 	args = append(args, "--gzip")
 	args = append(args, "--drop")
-	args = append(args, fmt.Sprintf("--archive=%s", file))
+	args = append(args, fmt.Sprintf("--archive=%q", file))
 
 	command := exec.Command("mongorestore", args...)
 	var stderr bytes.Buffer
 	command.Stderr = &stderr
 	if err := command.Run(); err != nil {
-		return fmt.Errorf("Failed to restore database: %s\n%v", stderr.String(), err)
+		return fmt.Errorf("Failed to restore database collection: %s\n%v", stderr.String(), err)
 	}
 	return nil
 }
