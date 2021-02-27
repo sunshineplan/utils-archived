07998f7b70521a54e5392cb21375485fbe60549a
sunshineplan <sunshineplan@users.noreply.github.com>
2021-02-27 10:18:11 +0800

    Add ErrNoMoreRetry error

--- retry.go
+++ retry.go
@@ -1,17 +1,24 @@
 package utils
 
-import "time"
+import (
+	"errors"
+	"time"
+)
+
+// ErrNoMoreRetry tells function does no more retry.
+var ErrNoMoreRetry = errors.New("No more retry")
 
 // Retry keeps retrying the function until no error is returned.
 func Retry(fn func() error, attempts, delay uint) (err error) {
-	var i uint
-	for ; i < attempts; i++ {
-		if err = fn(); err == nil {
+	for i := uint(0); i < attempts; i++ {
+		if err = fn(); err == nil || err == ErrNoMoreRetry {
 			return
 		}
+
 		if i < attempts-1 {
 			time.Sleep(time.Second * time.Duration(delay))
 		}
 	}
+
 	return
 }
--- retry_test.go
+++ retry_test.go
@@ -9,11 +9,12 @@ func TestRetry(t *testing.T) {
 	if err := Retry(func() error {
 		return nil
 	}, 3, 1); err != nil {
-		t.Error(err)
+		t.Error("expected nil error; got non-nil error")
 	}
+
 	if err := Retry(func() error {
 		return errors.New("error")
 	}, 3, 1); err == nil {
-		t.Error("gave nil error; want error")
+		t.Error("expected non-nil error; got nil error")
 	}
 }
